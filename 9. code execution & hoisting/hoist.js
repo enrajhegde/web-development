ea1c850fdf38510aafb5c3acaae4916a64bc2209
Kanishk Agrawal <kanishk71@gmail.com>
2025-02-17 08:56:17 +0530

    Code execution

---
+++ 9. code execution & hoisting/hoist.js
@@ -0,0 +1,20 @@
+// console.log(a); // undefined.
+// printHello(); // hello
+// var a = 1;
+// function printHello() {
+//     console.log('hello')
+// };
+
+console.log(a);
+printHello();
+printAge();
+
+var a = 1;
+function printHello() {
+    console.log('hello')
+};
+
+let printAge = function () {
+    console.log(20)
+}
+
