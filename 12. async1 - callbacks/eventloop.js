0327598b0955e8d697cbf7aa49e499e27afe9a53
Kanishk Agrawal <kanishk71@gmail.com>
2025-02-24 08:45:09 +0530

    event loop

---
+++ 12. async1 - callbacks/eventloop.js
@@ -0,0 +1,13 @@
+console.log("start");
+
+setTimeout(function () {
+    console.log("timer1")
+}, 0)
+
+setTimeout(function () {
+    console.log("timer2")
+}, 0)
+
+console.log("end")
+
+// start end timer2 timer1
